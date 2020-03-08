6eb5ff86929ed6634018117d0ead95816e70ac25
romluc <romulofilhofilho@gmail.com>
2020-03-08 09:32:00 -0300

    Set the routes

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+// routes.js
+const express = require('express');
+const routes = express.Router();
+
+const ProductController = require('./controllers/ProductController');
+
+// Route
+routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
+routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
+
+module.exports = routes;
