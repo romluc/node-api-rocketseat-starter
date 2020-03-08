04bb3ae34c235ac94b3f8ceaefd866bd127957bb
romluc <romulofilhofilho@gmail.com>
2020-03-08 09:59:32 -0300

    Add cors

--- server.js
+++ server.js
@@ -3,12 +3,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 const PORT = 3000;
 
 // Initialize app
 const app = express();
 app.use(express.json());
+app.use(cors);
 
 // Initialize database
 mongoose.connect('mongodb://localhost:27017/nodeapi', {
